4ce064e7536d5c0cb4b947816035f00dc2f75961
Boris Tronquoy <b.tronquoy@gmail.com>
2019-04-10 00:15:18 +0200

    feat(Footer): Add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="footer">
+    <v-container>
+      <p>
+      Made with <v-icon>favorite</v-icon> by Boris <strong>Tronquoy</strong> | #firestore #vuetify
+      </p>
+    </v-container>
+   
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    copyright: 'hello'
+  })
+}
+</script>
+
+<style>
+footer {
+ position: absolute;
+ bottom: 0;
+ width: 100%;
+ background: none
+}
+</style>
