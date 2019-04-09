0f604bd112ec77cf4d87322e5dc5014f56689e7b
Boris Tronquoy <b.tronquoy@gmail.com>
2019-04-10 00:17:42 +0200

    fix(Delete user): fix issue with deleting wrong user

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -3,15 +3,15 @@
     <h1>Component UserList</h1>
     <v-card class="elevation-12">
       <v-layout row wrap class="pa-3" v-for="user in sortedUsers" :key="user.id">
-        <v-flex xs6 sm4 md2>
+        <v-flex xs2>
           <div class="caption grey--text">Firstname</div>
           <div>{{ user.firstname }}</div>
         </v-flex>
-        <v-flex xs6 sm4 md2>
+        <v-flex xs2>
           <div class="caption grey--text">Lastname</div>
           <div>{{ user.lastname }}</div>
         </v-flex>
-        <v-flex xs6>
+        <v-flex xs3>
           <div class="caption grey--text">Job title</div>
           <div>{{ user.jobtitle }}</div>
         </v-flex>
@@ -22,7 +22,21 @@
                 <v-icon
                 color="primary"
                 dark v-on="on"
-                @click="del(user)"
+                @click="onEdit(user)"
+                >
+                  edit
+                </v-icon>
+              </v-btn>
+            </template>
+            <span>Edit</span>
+          </v-tooltip>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn flat icon color="red">
+                <v-icon
+                color="primary"
+                dark v-on="on"
+                @click="onDelete(user)"
                 >
                   delete_forever
                 </v-icon>
@@ -47,26 +61,20 @@ export default {
     }
   },
   methods: {
-    del: function(user) {
+    onDelete: function(user) {
       db.collection("users")
-        .where('firstname', '==', user.firstname).get
-          ()
-        .then(querySnapshot => {
-          querySnapshot.forEach(doc => {
-            doc.ref.delete().then(this.users)
-          })
-        })
-    },
+        .doc(user.id).delete()
+    }
   },
   computed:{
     sortedUsers () {
       return this.users.slice().sort(function(a, b){
-        var nameA=a.firstname.toLowerCase(), nameB=b.firstname.toLowerCase();
-        if (nameA < nameB)
-          return -1;
-        if (nameA > nameB)
-          return 1;
-        return 0;
+      //  var nameA=a.firstname.toLowerCase(), nameB=b.firstname.toLowerCase();
+      //   if (nameA < nameB)
+      //     return -1;
+      //   if (nameA > nameB)
+      //     return 1;
+      //   return 0;
       });
     }
   },
@@ -74,15 +82,21 @@ export default {
     db.collection('users').onSnapshot(res =>{
       const changes = res.docChanges();
       changes.forEach(change => {
+
         if (change.type === 'added'){
           this.users.push({
             ...change.doc.data(),
             id: change.doc.id
           })
         }
-        if (change.type === 'removed'){
-          const index = this.users.map(e => e.firstname).indexOf(change.doc.data().firstname);
-          this.users.splice(index, 1)
+
+        if (change.type === "removed") {
+          const deletedUser = this.users.find(
+            user => user.id === change.doc.id
+          );
+          const index = this.users.indexOf(deletedUser);
+          this.users.splice(index, 1);
+          this.index = this.index === 0 ? 0 : index - 1;
         }
       })
     })
