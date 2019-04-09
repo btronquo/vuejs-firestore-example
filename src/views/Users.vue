5468a81d8366715eeb65a74cd7b0f4979a7fa454
Boris Tronquoy <b.tronquoy@gmail.com>
2019-04-10 00:16:20 +0200

    fix(Style): fix some styles issue

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="users">
-    <div class="pa-3">
-      <h1 >vuejs-firestore-example</h1>
-      <p>Hi, I made this quick project to better understand how firestore and vuejs can work together.<br>
-        Free feel to contact me on <b>github</b> if you have any question! <br>
-        <b>Boris Tronquoy</b>
-      </p>
-    </div>
     <v-container class="my-5">
+      <div class="pa-3">
+        <h1 >vuejs-firestore-example</h1>
+        <p>Hi, I made this quick project to better understand how firestore and vuejs can work together.<br>
+          Free feel to contact me on <b>github</b> if you have any question! <br>
+          <b>Boris Tronquoy</b>
+        </p>
+      </div>
       <v-layout row justify-center>
-      <v-flex xs4>
+      <v-flex xs4 pa-3>
       <UserForm/>
       </v-flex>
-      <v-flex xs8>
+      <v-flex xs8 pa-3>
       <UserList/>
       </v-flex>
       </v-layout>
